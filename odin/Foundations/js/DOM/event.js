fca2a598aa7023986ed2fe0a2121368445b7c5a1
Sumedh-Fulkar <fulkarsumedh@gmail.com>
2025-10-03 11:42:02 +0530

    DOM-event-manipulation

---
+++ odin/Foundations/js/DOM/event.js
@@ -0,0 +1,30 @@
+
+// const btn = document.querySelector("#btn");
+// btn.onclick = () => alert("Hello World!");
+
+// const btn = document.querySelector("#btn");
+// btn.addEventListener("click", () => {
+//   alert("Hello World!");
+// });
+
+// the JavaScript file
+// METHOD 1
+// function alertFunction() {
+//   alert("YAY! YOU DID IT!");
+// }
+
+// the JavaScript file
+// METHODS 2 & 3
+// function alertFunction() {
+//   alert("YAY! YOU DID IT!");
+// }
+// const btn = document.querySelector("#btn");
+
+// METHOD 2
+// btn.onclick = alertFunction;
+
+// METHOD 3
+// btn.addEventListener("click", alertFunction);
+
+
+
