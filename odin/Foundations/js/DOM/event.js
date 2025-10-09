432762c9508e39558fba1f3ca73e51e1b5a99ec9
Sumedh-Fulkar <fulkarsumedh@gmail.com>
2025-10-09 17:52:47 +0530

    DOM exercise

--- odin/Foundations/js/DOM/event.js
+++ odin/Foundations/js/DOM/event.js
@@ -23,3 +23,28 @@ const btn = document.querySelector("#btn");
 
 // method3
 btn.addEventListener("click", alertFunction);
+
+btn.addEventListener("click", function (e) {
+  console.log(e);
+  console.log(e.target);
+});
+
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+textBox.addEventListener("keydown", (event) => {
+  output.textContent = `You pressed "${event.key}".`;
+});
+
+
+
+const btn1 = document.querySelector("#btn1");
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+function bgChange(e) {
+  const rndCol = `rgb(${random(255)} ${random(255)} ${random(255)})`;
+  e.target.style.backgroundColor = rndCol;
+  console.log(e);
+}
+
+btn1.addEventListener("click", bgChange);
